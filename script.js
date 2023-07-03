ae3dd4867852bf03af49cac54654bd5e9fb4874f
Julia Loftin <julia.loftin7@gmail.com>
2023-07-03 12:34:01 -0500

    formSubmission function validates there is input for pilot

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
-// Write your JavaScript code here!
+// // Write your JavaScript code here!
+// const { formSubmission } = require("./scriptHelper");
 
-window.addEventListener("load", function() {
+// const { formSubmission } = require("./scriptHelper");
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+// const { formSubmission } = require("./scriptHelper");
+
+// const { formSubmission } = require("./scriptHelper");
+
+    // let pilotInput = document.getElementById("input[name=pilotName]");
+    // let copilotInput = document.getElementById("input[name=copilotName]");
+    // let fuelLevelInput = document.getElementById("input[name=fuelLevel]");
+    // let cargoMassInput = document.getElementById("input[name=cargoMass]");
+
+window.addEventListener("load", function () {
+
+    const form = document.querySelector("form");
+    // form.addEventListener("submit", formSubmission)
+    let listedPlanets;
+    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+    let listedPlanetsResponse;
+    // listedPlanetsResponse.then(function (result) {
+    //     listedPlanets = result;
+    //     console.log(listedPlanets);
+    // }).then(function () {
+    //     console.log(listedPlanets);
+    //     // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+    
+    let pilotInput = document.querySelector("input[name=pilotName]");
+    form.addEventListener("submit", function(event){
+        formSubmission(document, pilotInput.value);
+    });
+    // })
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -2,33 +2,50 @@
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   */
+    // Here is the HTML formatting for our mission target div.
+    /*
+                 <h2>Mission Destination</h2>
+                 <ol>
+                     <li>Name: </li>
+                     <li>Diameter: </li>
+                     <li>Star: ${star}</li>
+                     <li>Distance from Earth: </li>
+                     <li>Number of Moons: </li>
+                 </ol>
+                 <img src="">
+    */
 }
 
 function validateInput(testInput) {
-   
+    if (!testInput) {
+        return "Empty";
+
+    } else if (isNaN(testInput)) {
+        return "Not a Number";
+
+    } else if (isNaN(testInput) === false) {
+        return "Is a Number";
+    }
 }
 
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+// you will use validateInput() to complete the formSubmission() function. formSubmission() will take in a document 
+// parameter and strings representing the pilot, co-pilot, fuel level, and cargo mass.
+
+// rest of if statement that checks that no inputs are empty
+//  || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty"
+
+function formSubmission(document, pilot) {
+    let form = document.querySelector("form");
+    if(validateInput(pilot) === "Empty"){
+        alert("Empty field!");
+    };
 }
 
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+    planetsReturned = await fetch().then(function (response) {
+    });
 
     return planetsReturned;
 }
@@ -39,5 +56,5 @@ function pickPlanet(planets) {
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
-module.exports.pickPlanet = pickPlanet; 
+module.exports.pickPlanet = pickPlanet;
 module.exports.myFetch = myFetch;
